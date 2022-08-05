8854341342c7fa273e5f4e9517713d55d9e2f106
GwendolinD <dubois.gwendoline@hotmail.fr>
2022-08-05 11:44:34 +0200

    add footer by gwendoline

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 console.log("Hello world !");
-console.log("Hello from github !!!")
+console.log("Hello from github !!!");
 
 function App() {
   return (
     <div className="App">
       <h1>GIT/GITHUB</h1>
       <Header />
+      <Footer />
     </div>
   );
 }
